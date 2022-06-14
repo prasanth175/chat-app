3784833ddaece4ae7dd963208ae38e8a5c594a3b
Prasanth <1022prasanthkumar@gmail.com>
2022-06-14 09:24:29 +0530

    added react router

--- React3/box-office/src/App.jsx
+++ React3/box-office/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path = "/starred">
+        This is starred
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
