a4593b44c002b7c9d8fb271960c0d9da3d36db47
Prasanth <1022prasanthkumar@gmail.com>
2022-06-14 23:29:48 +0530

    created input and fetch results from tv maze api

--- React3/box-office/src/components/MainProjectLayout.jsx
+++ React3/box-office/src/components/MainProjectLayout.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Navs from './Navs';
 import Title from './Title';
 
-function MainProjectLayout({ Children }) {
+function MainProjectLayout({ children }) {
   return (
     <div>
         <Title 
         title="Box Office" 
-        subtitle="Are you looking for a movie or an actor" />
-    <Navs />
+        subtitle="Are you looking for a movie or an actor?" />
+        <Navs />
+        {children}
 
-    {Children}
+        
   </div>
   )
 }
--- React3/box-office/src/pages/Home.jsx
+++ React3/box-office/src/pages/Home.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainProjectLayout from '../components/MainProjectLayout';
 
 
+
 function Home() {
+
+  const [input, setInput] = useState('');
+
+  const onSearch = () =>{
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    .then(r => r.json())
+    .then(result => {
+      console.log(result);
+    })
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if(ev.keyCode === 13) {
+      onSearch()
+    }
+  };
+
   return (
-    <div>
-      <MainProjectLayout />
-      <p>This is home</p>
-    </div>
+      <MainProjectLayout >
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+      <button type="button" onClick={onSearch}>Search</button>
+      </MainProjectLayout>
+    
 )
-}
+  }
 
 export default Home;
