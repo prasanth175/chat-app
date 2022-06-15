94de214a33253a24ab5c7ba2ac0c2f8c747d602b
Prasanth <1022prasanthkumar@gmail.com>
2022-06-15 19:23:48 +0530

    added search options

--- React3/box-office/src/pages/Home.jsx
+++ React3/box-office/src/pages/Home.jsx
@@ -8,9 +8,12 @@ function Home() {
 
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onSearch = () =>{
-    apiGet(`/Search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     
     })
@@ -26,6 +29,10 @@ function Home() {
     }
   };
 
+  const onRadioChange = (ev) => {
+    setSearchOption(ev.target.value)
+  }
+
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div>No results</div>
@@ -33,14 +40,13 @@ function Home() {
 
 
   if (results && results.length > 0) {
-    return (
-      <div>
-        {results.map((item => 
-          <div key={item.show.id}>{item.show.name}</div>
-          ))}
-      </div>
-    )
+    return results[0].show ? results.map(item => 
+        <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+        <div key={item.person.id}>{item.person.name}</div>
+        ));
   }
+ 
 
   return null;
 }
@@ -49,7 +55,25 @@ function Home() {
 
   return (
       <MainProjectLayout >
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+      <input type="text"
+       placeholder='Search for something'
+        onChange={onInputChange}
+         onKeyDown={onKeyDown}
+          value={input} 
+          />
+
+          <div>
+            <label htmlFor="shows-search">
+              shows
+              <input id='shows-search' type="radio" value="shows" checked={isShowsSearch} onChange={onRadioChange} />
+            </label>
+
+            <label htmlFor="actors-search">
+              Actors
+              <input id='actors-search' type="radio" value="people" checked={!isShowsSearch} onChange={onRadioChange} />
+            </label>
+          </div>
+
       <button type="button" onClick={onSearch}>Search</button>
       {renderResults()}
       </MainProjectLayout>
