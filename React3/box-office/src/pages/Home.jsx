c09983f8ce9f63034967ad115397371d7e7e7edf
Prasanth <1022prasanthkumar@gmail.com>
2022-06-17 18:42:35 +0530

    created two new more hooks: useLastQuery and useShow

--- React3/box-office/src/pages/Home.jsx
+++ React3/box-office/src/pages/Home.jsx
@@ -3,12 +3,13 @@ import ActorGrid from '../components/actor/ActorGrid';
 import MainProjectLayout from '../components/MainProjectLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import { useLastQuery } from '../misc/custom-hooks';
 
 
 
 function Home() {
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
