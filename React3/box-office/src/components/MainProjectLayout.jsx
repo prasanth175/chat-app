bc92c355ae5a067564feb8c1c796c0b8ddf17636
Prasanth <1022prasanthkumar@gmail.com>
2022-06-17 18:02:25 +0530

    finished starred page

--- React3/box-office/src/components/MainProjectLayout.jsx
+++ React3/box-office/src/components/MainProjectLayout.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import Navs from './Navs';
 import Title from './Title';
 
-function MainProjectLayout({ children }) {
+function MainPageLayout({ children }) {
   return (
     <div>
-        <Title 
-        title="Box Office" 
-        subtitle="Are you looking for a movie or an actor?" />
-        <Navs />
-        {children}
+      <Title
+        title="Box Office"
+        subtitle="Are you looking for a movie or an actor?"
+      />
+      <Navs />
 
-        
-  </div>
-  )
-}
+      {children}
+    </div>
+  );
+};
 
-
-export default MainProjectLayout;
+export default MainPageLayout;
