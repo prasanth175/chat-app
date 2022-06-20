3a0800d39640d71b733566f478f212f3988903e6
Prasanth <1022prasanthkumar@gmail.com>
2022-06-20 09:56:48 +0530

    scaffold the app

---
+++ React4/chat-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div>Hi
+    </div>
+  );
+}
+
+export default App;
