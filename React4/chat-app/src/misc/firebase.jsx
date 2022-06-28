d942d1be8b8a25f0bd603714de25770470424fdc
Prasanth <1022prasanthkumar@gmail.com>
2022-06-28 11:33:06 +0530

    initialized firebase project

---
+++ React4/chat-app/src/misc/firebase.jsx
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAeBsJrRseLRbEgyafJqqG5tIS1YEEdu6U",
+    authDomain: "chat-web-app-685d5.firebaseapp.com",
+    databaseURL: "https://chat-web-app-685d5-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-685d5",
+    storageBucket: "chat-web-app-685d5.appspot.com",
+    messagingSenderId: "943062344409",
+    appId: "1:943062344409:web:3c22da0a746da76dcc54c8"
+  };
+
+const app =  firebase.initializeApp(config);
