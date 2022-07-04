c709f9b7c22a5c8fe95eea1d2a0ea4bd3d621bbe
Prasanth <1022prasanthkumar@gmail.com>
2022-07-04 15:59:46 +0530

    started building the sidebar component

---
+++ React4/chat-app/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import DashboardToggle from './dashboard/DashboardToggle';
+
+function Sidebar() {
+  return (
+    <div>
+
+
+        <div>
+            <DashboardToggle />
+        </div>
+
+        bottom
+
+
+    </div>
+  )
+}
+
+export default Sidebar;
