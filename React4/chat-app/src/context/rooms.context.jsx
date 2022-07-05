0e05b9996bb6c5a3d4644e1e6358236065c69855
Prasanth <1022prasanthkumar@gmail.com>
2022-07-05 16:36:41 +0530

    created rooms context

---
+++ React4/chat-app/src/context/rooms.context.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useEffect, useState } from "react";
+import { database } from "../misc/firebase";
+import { transformToArrWithId } from '../misc/helpers';
+
+
+
+const RoomsContext = createContext();
+
+function RoomsProvider({children}) {
+    const [rooms, setRooms] = useState(null);
+
+    useEffect(() => {
+        const roomListRef = database.ref('rooms');
+
+        roomListRef.on('value', snap => {
+            const data = transformToArrWithId(snap.val())
+            setRooms(data);
+        });
+
+        return () => {
+            roomListRef.off();
+        }
+    }, []);
+
+    return (
+        <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+    )
+}
+
+export default RoomsProvider;
+
