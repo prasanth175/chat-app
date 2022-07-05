b29a65c9cc72872bd3889701ec005ca998dcddb4
Prasanth <1022prasanthkumar@gmail.com>
2022-07-05 09:33:43 +0530

    Started to work with user avatar uploasd

--- React4/chat-app/src/components/dashboard/index.jsx
+++ React4/chat-app/src/components/dashboard/index.jsx
@@ -4,6 +4,7 @@ import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
 
 function Dashboard({ onSignOut }) {
@@ -44,6 +45,7 @@ function Dashboard({ onSignOut }) {
         onSave={onSave}
         label={<h6 className='mb-2'>Nickname</h6>}
       />
+      <AvatarUploadBtn />
     </Drawer.Body>
 
     <Drawer.Footer>
