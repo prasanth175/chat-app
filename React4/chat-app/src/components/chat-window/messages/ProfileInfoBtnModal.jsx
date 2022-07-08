94327f5345f054edbdf6b9db421f94c74ba369ec
Prasanth <1022prasanthkumar@gmail.com>
2022-07-08 12:34:59 +0530

    added button to grant or revoke admin access

--- React4/chat-app/src/components/chat-window/messages/ProfileInfoBtnModal.jsx
+++ React4/chat-app/src/components/chat-window/messages/ProfileInfoBtnModal.jsx
@@ -3,7 +3,7 @@ import { Button, Modal } from 'rsuite';
 import { useModalState } from '../../../misc/custom-hooks';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
 
-function ProfileInfoBtnModal({ profile, ...btnProps }) {
+function ProfileInfoBtnModal({ profile, children, ...btnProps }) {
 
     const { isOpen, close, open } = useModalState()
 
@@ -35,6 +35,7 @@ function ProfileInfoBtnModal({ profile, ...btnProps }) {
 
             </Modal.Body>
             <Modal.Footer>
+                {children}
                 <Button block onClick={close}>
                     Close
                 </Button>
