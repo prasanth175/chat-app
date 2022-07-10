7df1addbb0c23abb1688acf6a0811e360322f4e5
Prasanth <1022prasanthkumar@gmail.com>
2022-07-10 20:47:12 +0530

    setup cloud functions with num

---
+++ React4/chat-app/functions/index.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chat-web-app-685d5-default-rtdb.firebaseio.com"
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
